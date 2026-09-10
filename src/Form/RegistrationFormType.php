c5f945d83c7f64c9bd0cb4f3e0c749d056452f1b
delacroix <delacroix.jon@gmail.com>
2026-09-10 12:50:24 +0200

    Contrainte mot de passe

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -12,6 +12,7 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Regex;
 
 class RegistrationFormType extends AbstractType
 {
@@ -29,9 +30,13 @@ class RegistrationFormType extends AbstractType
                 'constraints' => [
                     new NotBlank(['message' => 'Veuillez saisir un mot de passe.']),
                     new Length([
-                        'min' => 6,
+                        'min' => 10,
                         'minMessage' => 'Votre mot de passe doit contenir au moins {{ limit }} caractères.',
                         'max' => 4096,
+                    ]),
+                    new Regex([
+                        'pattern' => '/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).+$/',
+                        'message' => 'Le mot de passe doit contenir au moins une majuscule, une minuscule, un chiffre et un caractère spécial.',
                     ]),
                 ],
             ])
